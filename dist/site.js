646f5e359429adc862dc09fc87ca0e3dd71b8550
Andrew Petersen <andrew.petersen15@gmail.com>
2016-12-28 18:03:32 -0600

    scroll to top on navigate

--- dist/site.js
+++ dist/site.js
@@ -485,6 +485,7 @@
 	
 	            hub.state.activeView = views.findByPath(path);
 	            hub.state.activeView.init(hub.state);
+	            window.scroll(0, 0);
 	            hub.state.menu.activePath = hub.state.activeView.path;
 	            leftMenu.setActive();
 	
