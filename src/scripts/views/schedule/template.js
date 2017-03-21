955057b528b4b24cdc9c13c1c5a2089d6e9bf067
Andrew Petersen <andrew.petersen15@gmail.com>
2017-03-21 18:03:45 -0500

    added schedule styling

--- src/scripts/views/schedule/template.js
+++ src/scripts/views/schedule/template.js
@@ -6,7 +6,7 @@ exports.render = function(model) {
                 <h2 class='view-title'>Schedule</h2>
             </div>
             <div class='events'>
-                ${model.map(renderEvent).join("\n")}
+                ${model.map(renderEvent).join("\n<hr>\n")}
             </div>
         </div>
     `
@@ -15,8 +15,8 @@ exports.render = function(model) {
 var renderEvent = function(event) {
     return `
 <div class='event'>
-    <h2 class='title'>${event.title}</h2>
     <div class='date'>${event.date}</div>
+    <h2 class='title'>${event.title}</h2>
     <h3 class='time'>${event.time}</h3>
     <div class='location'>${event.location}</div>
     <p class='details'>${event.details}</p>
