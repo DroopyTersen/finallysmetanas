cbd14e442de0896ff6ed80f493eabe1a6cfc9167
Andrew Petersen <andrew.petersen15@gmail.com>
2017-03-22 09:19:14 -0500

    roughly styled travel page

--- src/scripts/views/travel/template.js
+++ src/scripts/views/travel/template.js
@@ -8,10 +8,13 @@ exports.render = function(model) {
             <div class='content'>
                 <h2>Accomodations</h2>
                 <div>
-                    <h4><a href='http://www.marriott.com/meeting-event-hotels/group-corporate-travel/groupCorp.mi?resLinkData=Nash/Smetana%20Wedding%20Room%20Block%5Emkede%60nasnasa%60129%60USD%60false%605%6011/24/17%6011/26/17%6010/24/17&app=resvlink&stop_mobi=yes'
-                            target='_blank'>Racine Architect Hotel & Conference Center</a><h4>
-                    <p>Book by 10/24/2017</p>
-                    <p>Shuttles will be provided before the ceremony at 3:30pm and after beginning at 11:00pm</p>
+                    <h4 class='place'>
+                        <a 
+                            href='http://www.marriott.com/meeting-event-hotels/group-corporate-travel/groupCorp.mi?resLinkData=Nash/Smetana%20Wedding%20Room%20Block%5Emkede%60nasnasa%60129%60USD%60false%605%6011/24/17%6011/26/17%6010/24/17&app=resvlink&stop_mobi=yes'
+                            target='_blank'>Racine Architect Hotel & Conference Center</a>
+                    </h4>
+                    <p class='book-by'>Book by 10/24/2017</p>
+                    <p class='details'>Shuttles will be provided before the ceremony at 3:30pm and after beginning at 11:00pm</p>
                 </div>
             </div>
         </div>
